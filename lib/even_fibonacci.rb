b0e111fbc2120b692f717b2abea11781623cb84b
hpetrow <heather.petrow@gmail.com>
2015-07-02 09:52:25 -0400

    done

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,22 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(n)
+  fibonacci(n).inject(0) { |sum, n|
+    if (n % 2 == 0)
+      sum + n
+    else
+      sum
+    end
+  }
+end
+
+def fibonacci(n)
+  sequence = [1, 1]
+  next_term = sequence[-1] + sequence[-2]
+
+  until next_term >= n
+    sequence << next_term
+    next_term = sequence[-1] + sequence[-2]
+  end
+
+  sequence
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  attr_reader :max
+
+  def initialize(max)
+    @max = max
+  end
+
+  def sum
+    self.fibonacci.inject(0) { |sum, n|
+      if (n % 2 == 0)
+        sum + n
+      else
+        sum
+      end
+    }
+  end
+
+  def fibonacci
+    sequence = [1, 1]
+    next_term = sequence[-1] + sequence[-2]
+
+    until next_term >= self.max
+      sequence << next_term
+      next_term = sequence[-1] + sequence[-2]
+    end
+
+    sequence
+  end
+end
